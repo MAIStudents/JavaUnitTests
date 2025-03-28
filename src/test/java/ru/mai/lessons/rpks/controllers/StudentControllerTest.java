174887db895d600122c139b8cf5adbe6433e2e92
ykwais <fedorkovaleks04@gmail.com>
2025-03-28 05:25:16 +0300

    ok

--- src/test/java/ru/mai/lessons/rpks/controllers/StudentControllerTest.java
+++ src/test/java/ru/mai/lessons/rpks/controllers/StudentControllerTest.java
@@ -1,7 +1,7 @@
 package ru.mai.lessons.rpks.controllers;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -11,10 +11,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import org.webjars.NotFoundException;
 import ru.mai.lessons.rpks.controllers.impl.StudentControllerImpl;
+import ru.mai.lessons.rpks.dto.requests.StudentCreateRequest;
+import ru.mai.lessons.rpks.dto.requests.StudentUpdateRequest;
 import ru.mai.lessons.rpks.dto.respones.StudentResponse;
 import ru.mai.lessons.rpks.services.StudentService;
 import ru.mai.lessons.rpks.utils.JsonUtils;
@@ -30,19 +35,127 @@ class StudentControllerTest {
   @MockitoBean
   private StudentService service;
 
+  private final Long studentId = 1L;
+  private final StudentResponse studentResponse = new StudentResponse(studentId, "Fedorkov", "M8O-311Б");
+  private final StudentCreateRequest createRequest = new StudentCreateRequest("Fedorkov", "M8O-311Б");
+  private final StudentCreateRequest invalidCreateRequest = new StudentCreateRequest(null, null);
+  private final StudentUpdateRequest updateRequest = new StudentUpdateRequest(studentId, "Fedorkov Alex", "M8O-311Б");
+
+  @Test
+  @SneakyThrows
+  @DisplayName("Тест на успешное сохранение студента")
+  void givenValidRequest_whenSaveStudent_thenReturnOk() {
+    when(service.saveStudent(createRequest)).thenReturn(studentResponse);
+
+    mockMvc
+            .perform(
+                    post("/student/save")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(JsonUtils.toJson(createRequest))
+            )
+            .andExpect(status().isOk())
+            .andExpect(content().string(JsonUtils.toJson(studentResponse)));
+  }
+
+  @Test
+  @SneakyThrows
+  @DisplayName("Тест на неуспешное сохранение студента с невалидными данными")
+  void givenInvalidRequest_whenSaveStudent_thenReturnUnprocessableEntity() {
+    mockMvc
+            .perform(
+                    post("/student/save")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(JsonUtils.toJson(invalidCreateRequest))
+            )
+            .andExpect(status().isUnprocessableEntity());
+  }
+
   @Test
   @SneakyThrows
   @DisplayName("Тест на поиск студента по его идентификатору")
   void givenStudentId_whenGetStudent_thenReturnStudentResponse() {
-    StudentResponse expectedResponse = new StudentResponse();
-    when(service.getStudent(1L)).thenReturn(expectedResponse);
+    when(service.getStudent(1L)).thenReturn(studentResponse);
 
     mockMvc
         .perform(
             get("/student/get")
-                .param("id", "1")
+                .param("id", studentId.toString())
         )
         .andExpect(status().isOk())
-        .andExpect(content().string(JsonUtils.toJson(expectedResponse)));
+        .andExpect(content().string(JsonUtils.toJson(studentResponse)));
+  }
+
+  @Test
+  @SneakyThrows
+  @DisplayName("Тест на неуспешный поиск студента с несуществующим ID")
+  void givenInvalidId_whenGetStudent_thenReturnUnprocessableEntity() {
+    when(service.getStudent(studentId)).thenThrow(NotFoundException.class);
+
+    mockMvc
+            .perform(
+                    get("/student/get")
+                            .param("id", studentId.toString())
+            )
+            .andExpect(status().isUnprocessableEntity());
+  }
+
+  @Test
+  @SneakyThrows
+  @DisplayName("Тест на успешное обновление студента")
+  void givenValidRequest_whenUpdateStudent_thenReturnOk() {
+    when(service.updateStudent(updateRequest)).thenReturn(studentResponse);
+
+    mockMvc
+            .perform(
+                    put("/student/update")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(JsonUtils.toJson(updateRequest))
+            )
+            .andExpect(status().isOk())
+            .andExpect(content().string(JsonUtils.toJson(studentResponse)));
+  }
+
+  @Test
+  @SneakyThrows
+  @DisplayName("Тест на неуспешное обновление несуществующего студента")
+  void givenInvalidRequest_whenUpdateStudent_thenReturnUnprocessableEntity() {
+    when(service.updateStudent(updateRequest)).thenThrow(NotFoundException.class);
+
+    mockMvc
+            .perform(
+                    put("/student/update")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(JsonUtils.toJson(updateRequest))
+            )
+            .andExpect(status().isUnprocessableEntity());
+  }
+
+  @Test
+  @SneakyThrows
+  @DisplayName("тест на успешное удаление студента")
+  void givenValidId_whenDeleteStudent_thenReturnOk() {
+    when(service.deleteStudent(studentId)).thenReturn(studentResponse);
+
+    mockMvc
+            .perform(
+                    delete("/student/delete")
+                            .param("id", studentId.toString())
+            )
+            .andExpect(status().isOk())
+            .andExpect(content().string(JsonUtils.toJson(studentResponse)));
+  }
+
+  @Test
+  @SneakyThrows
+  @DisplayName("тест на неуспешное удаление несуществующего студента")
+  void givenInvalidId_whenDeleteStudent_thenReturnUnprocessableEntity() {
+    when(service.deleteStudent(studentId)).thenThrow(NotFoundException.class);
+
+    mockMvc
+            .perform(
+                    delete("/student/delete")
+                            .param("id", studentId.toString())
+            )
+            .andExpect(status().isUnprocessableEntity());
   }
 }
--- src/test/java/ru/mai/lessons/rpks/services/StudentServiceTest.java
+++ src/test/java/ru/mai/lessons/rpks/services/StudentServiceTest.java
@@ -1,6 +1,7 @@
 package ru.mai.lessons.rpks.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -10,7 +11,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.webjars.NotFoundException;
 import ru.mai.lessons.rpks.dto.mappers.StudentMapper;
+import ru.mai.lessons.rpks.dto.requests.StudentCreateRequest;
+import ru.mai.lessons.rpks.dto.requests.StudentUpdateRequest;
 import ru.mai.lessons.rpks.dto.respones.StudentResponse;
 import ru.mai.lessons.rpks.models.Student;
 import ru.mai.lessons.rpks.repositories.StudentRepository;
@@ -28,17 +32,94 @@ class StudentServiceTest {
   @InjectMocks
   private StudentServiceImpl service;
 
+  private final Long studentID = 1L;
+  private final Student student = new Student(studentID, "Fedorkov", "M8O-311Б");
+  private final Student invalidStudent = new Student(null, null, null);
+  private final Student updatedStudent = new Student(studentID, "Fedorkov Alex", "M8O-311Б");
+  private final StudentResponse studentResponse = new StudentResponse(studentID, "Fedorkov", "M8O-311Б");
+  private final StudentResponse updatedStudentResponse = new StudentResponse(studentID, "Fedorkov Alex", "M8O-311Б");
+  private final StudentCreateRequest createRequest = new StudentCreateRequest("Fedorkov", "M8O-311Б");
+  private final StudentCreateRequest invalidCreateRequest = new StudentCreateRequest(null, null);
+  private final StudentUpdateRequest updateRequest = new StudentUpdateRequest(studentID, "Fedorkov Alex", "M8O-311Б");
+  private final StudentUpdateRequest invalidUpdateRequest = new StudentUpdateRequest(studentID, null, null);
+
+  @Test
+  @DisplayName("Тест на успешное сохранение студента")
+  void givenValidCreateRequest_whenSaveStudent_thenReturnResponse() {
+    when(mapper.requestToModel(createRequest)).thenReturn(student);
+    when(repository.saveAndFlush(student)).thenReturn(student);
+    when(mapper.modelToResponse(student)).thenReturn(studentResponse);
+
+    StudentResponse result = service.saveStudent(createRequest);
+
+    assertEquals(studentResponse, result);
+  }
+
+  @Test
+  @DisplayName("Тест на неуспешное сохранение студента ввиду невалидности его данных")
+  void givenInvalidCreateRequest_whenSaveStudent_thenThrowException() {
+    when(mapper.requestToModel(invalidCreateRequest)).thenReturn(invalidStudent);
+    when(repository.saveAndFlush(invalidStudent)).thenThrow(new IllegalArgumentException());
+
+    assertThrows(IllegalArgumentException.class, () -> service.saveStudent(invalidCreateRequest));
+  }
+
   @Test
   @DisplayName("Тест на поиск студента по его идентификатору")
   void givenStudentId_whenGetStudent_thenReturnStudentResponse() {
-    Long studentId = 1L;
-    StudentResponse expectedResponse = new StudentResponse(1L, "Domoroschenov", "М8О-411Б");
-    Student expectedModel = new Student(1L, "Domoroschenov", "М8О-411Б");
-    when(repository.findById(studentId)).thenReturn(Optional.of(expectedModel));
-    when(mapper.modelToResponse(expectedModel)).thenReturn(expectedResponse);
+    when(repository.findById(studentID)).thenReturn(Optional.of(student));
+    when(mapper.modelToResponse(student)).thenReturn(studentResponse);
 
-    StudentResponse actualResponse = service.getStudent(studentId);
+    StudentResponse actualResponse = service.getStudent(studentID);
 
-    assertEquals(expectedResponse, actualResponse);
+    assertEquals(studentResponse, actualResponse);
+  }
+
+  @Test
+  @DisplayName("Тест на поиск несуществующего студента")
+  void givenNotExistingStudentId_whenGetStudent_thenThrowException() {
+    when(repository.findById(studentID)).thenReturn(Optional.empty());
+
+    assertThrows(NotFoundException.class, () -> service.getStudent(studentID));
+  }
+
+  @Test
+  @DisplayName("Тест на успешное обновление")
+  void givenValidUpdateRequest_whenUpdateStudent_thenReturnUpdatedResponse() {
+    when(mapper.requestToModel(updateRequest)).thenReturn(updatedStudent);
+    when(repository.saveAndFlush(updatedStudent)).thenReturn(updatedStudent);
+    when(mapper.modelToResponse(updatedStudent)).thenReturn(updatedStudentResponse);
+
+    StudentResponse result = service.updateStudent(updateRequest);
+
+    assertEquals(updatedStudentResponse, result);
+  }
+
+  @Test
+  @DisplayName("Тест на неуспешное обновление студента")
+  void givenInvalidUpdateRequest_whenUpdateStudent_thenThrowException() {
+    when(mapper.requestToModel(invalidUpdateRequest)).thenReturn(invalidStudent);
+    when(repository.saveAndFlush(invalidStudent)).thenThrow(new IllegalArgumentException());
+
+    assertThrows(IllegalArgumentException.class, () -> service.updateStudent(invalidUpdateRequest));
+  }
+
+  @Test
+  @DisplayName("Тест на успешное удаление студента")
+  void givenExistingStudentId_whenDeleteStudent_thenReturnDeletedResponse() {
+    when(repository.findById(studentID)).thenReturn(Optional.of(student));
+    when(mapper.modelToResponse(student)).thenReturn(studentResponse);
+
+    StudentResponse result = service.deleteStudent(studentID);
+
+    assertEquals(studentResponse, result);
+  }
+
+  @Test
+  @DisplayName("Тест на неуспешное удаление студента")
+  void givenNotExistingId_whenDeleteStudent_thenThrowException() {
+    when(repository.findById(studentID)).thenReturn(Optional.empty());
+
+    assertThrows(NotFoundException.class, () -> service.deleteStudent(studentID));
   }
 }
